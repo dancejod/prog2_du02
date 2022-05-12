26db95aa06591abd8093ead5930d678a30643ba4
dancejod <dancejova.daniela@gmail.com>
2022-05-12 20:00:52 +0200

    Pridana kostra programu s prepojeniami, treba ziskat segmenty

--- ukol2_gtfs.py
+++ ukol2_gtfs.py
@@ -1,4 +1,5 @@
 import csv
+
 with open('PID_GTFS/stops.txt',encoding="utf-8", newline='') as raw_stops, open('PID_GTFS/stop_times.txt',encoding="utf-8", newline='') as raw_stop_times, open('PID_GTFS/trips.txt',encoding="utf-8", newline='') as raw_trips, open('PID_GTFS/routes.txt',encoding="utf-8", newline='') as raw_routes:
     stops_reader = csv.DictReader(raw_stops)
     stop_times_reader = csv.DictReader(raw_stop_times)
@@ -28,76 +29,74 @@ with open('PID_GTFS/stops.txt',encoding="utf-8", newline='') as raw_stops, open(
         our_data_routes[route_pk]={'kratke_jmeno':route_kratke_jmeno,'dlouhe_jmeno':route_dlouhe_jmeno}
 
     for row in stop_times_reader:
-        cesta=row['trip_id']
+        cesta=row['trip_id'] #matuce
         zastavka=row['stop_id']
         poradi=row['stop_sequence']
-        our_data_stop_times.append({'cesta':cesta,'zastavka':zastavka,'poradi':poradi})
-
-class Stop(object):
-    def __init__ (self):
-        self.id=None
-        self.name=None
-    
-    def get_data(self,data,pk):
-        self.id=pk
-        self.name=data[pk]['jmeno']
-        return self
-
-class Trip(object):
-    def __init__ (self):
-        self.id=None
-        self.route=None
-        #self.stops=[]
-
-    def get_data(self,data,data_routes,pk):
-        self.id=pk
-        pk_route=data[pk]['linka_id']
-        self.route=Route().get_data(data_routes,pk_route)
-        return self
-
+        prichod=row['arrival_time']
+        odchod=row['departure_time']
+        our_data_stop_times.append({'cesta':cesta,'zastavka':zastavka,'poradi':poradi, 'prichod': prichod, 'odchod': odchod})
 
 class Route(object):
-    def __init__(self):
-        self.id=None
-        self.short_name=None
-        self.long_name=None
-        #self.trips=[]
-
-    def get_data(self,data,pk):
-        self.id=pk
-        self.short_name=data[pk]['kratke_jmeno']
-        self.long_name=data[pk]['dlouhe_jmeno']
-        return self
+    def __init__(self,route_data, route_id):
+        self.id=route_id
+        self.short_name=route_data[route_id]['kratke_jmeno']
+        self.long_name=route_data[route_id]['dlouhe_jmeno']
+        #self.routes[route_id] = {'id:': self.id,'short_name:': self.short_name, 'long_name:': self.long_name}
 
     def __str__(self):
         return f"{id(self)}:{self.id}, {id(self)}:{self.short_name}, {id(self)}:{self.long_name}"
 
-class StopTime(object):
-    def __init__(self):
-        self.trip=None
-        self.stop=None
-        self.rank=None
+class Stop(object):
+    def __init__ (self, stop_id, stop_name):
+        self.id=stop_id
+        self.name=stop_name
 
-    def get_data(self,data,data_trips,data_stops,data_routes):  # this might be illegal
-        for i in data:
-            pk_trip=i['cesta']
-            pk_stop=i['zastavka']
-            self.trip=Trip().get_data(data_trips,data_routes,pk_trip)
-            self.stop=Stop().get_data(data_stops,pk_stop)
-            self.rank=i['poradi']
-            return self
+class Trip(object):
+    def __init__ (self, trip_id, route):
+        self.id=trip_id
+        self.route=route
+        self.stoptime_list = []
+
+    def add_stoptime(self, stoptime):
+        self.stoptime_list.append(stoptime)
+
+class StopTime(object):
+    def __init__(self, trip, stop, stop_sequence, deptime, arrtime):
+        self.trip=trip
+        self.stop=stop
+        self.rank=stop_sequence
+        self.departure=deptime
+        self.arrival=arrtime
+        self.trip.add_stoptime(self)
     
     def __str__(self):
         return f"{id(self)}:{self.trip}, {id(self)}:{self.stop}, {id(self)}:{self.rank}"
 
 class StopSegment(object):
-    def __init__(self):
-        self.from_stop=None
-        self.to_stop=None
-        self.trips=[] # z toho pak ziskame count
+    def __init__(self, trip, from_stop, to_stop):
+        self.trip = trip
+        self.from_stop=from_stop
+        self.to_stop=to_stop
+        self.trips_list=[] # z toho pak ziskame count
+        
+    def add_trip_to_list(self, trip):
+        self.trips_list.append(trip)
 
 
+first_stop = Stop(1, 'kakatka')
+second_stop = Stop(2, 'dankajesuper')
 
-#print(our_data_stops)
+first_route = Route({3:{
+    'kratke_jmeno':"d",
+    'dlouhe_jmeno':"ddddd"
+}},3)
+
+first_trip = Trip(100, first_route)
+
+stopkaa = StopTime(first_trip, first_stop, 1,'0700', '1400')
+
+segment = StopSegment(first_trip,first_stop,second_stop)
+
+print("kako")
 #print(StopTime().get_data(our_data_stop_times,our_data_trips,our_data_stops,our_data_routes))
-print(StopTime().get_data(our_data_stop_times,our_data_trips,our_data_stops,our_data_routes))
+#print(StopTime().get_data(our_data_stop_times,our_data_trips,our_data_stops,our_data_routes))
