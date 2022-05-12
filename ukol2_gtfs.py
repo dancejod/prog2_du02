424d78102ed22c7cddef778be1090ac00a3288d2
dancejod <dancejova.daniela@gmail.com>
2022-05-12 23:51:39 +0200

    Upravena architektura okrem StopTime, inicializovanie tried pri otvoreniach suborov

--- ukol2_gtfs.py
+++ ukol2_gtfs.py
@@ -1,55 +1,25 @@
 import csv
 
-with open('PID_GTFS/stops.txt',encoding="utf-8", newline='') as raw_stops, open('PID_GTFS/stop_times.txt',encoding="utf-8", newline='') as raw_stop_times, open('PID_GTFS/trips.txt',encoding="utf-8", newline='') as raw_trips, open('PID_GTFS/routes.txt',encoding="utf-8", newline='') as raw_routes:
-    stops_reader = csv.DictReader(raw_stops)
-    stop_times_reader = csv.DictReader(raw_stop_times)
-    trips_reader = csv.DictReader(raw_trips)
-    routes_reader = csv.DictReader(raw_routes)
-
-    our_data_stops={}
-    our_data_stop_times=[]
-    our_data_trips={}
-    our_data_routes={}
-
-    for row in stops_reader:
-        stop_pk=row['stop_id']
-        stop_jmeno=row['stop_name'] # Vo finalnej verzii len nezabudnut na cechizmy, dat po anglicky
-        our_data_stops[stop_pk]={'jmeno':stop_jmeno} # !! Niektore stops nemaju jmeno, ale to asi neva
-                                                    # Sample: 'U1072S1E1081': {'jmeno': 'E8'}
-    
-    for row in trips_reader:
-        trip_pk=row['trip_id']
-        linka_id=row['route_id']
-        our_data_trips[trip_pk]={'linka_id':linka_id} # Sample: '1349_28156_211212': {'linka_id': 'L1349'},
-
-    for row in routes_reader:
-        route_pk=row['route_id']
-        route_kratke_jmeno=row['route_short_name']
-        route_dlouhe_jmeno=row['route_long_name']     
-        our_data_routes[route_pk]={'kratke_jmeno':route_kratke_jmeno,'dlouhe_jmeno':route_dlouhe_jmeno}
-
-    for row in stop_times_reader:
-        cesta=row['trip_id'] #matuce
-        zastavka=row['stop_id']
-        poradi=row['stop_sequence']
-        prichod=row['arrival_time']
-        odchod=row['departure_time']
-        our_data_stop_times.append({'cesta':cesta,'zastavka':zastavka,'poradi':poradi, 'prichod': prichod, 'odchod': odchod})
-
-class Route(object):
-    def __init__(self,route_data, route_id):
-        self.id=route_id
-        self.short_name=route_data[route_id]['kratke_jmeno']
-        self.long_name=route_data[route_id]['dlouhe_jmeno']
-        #self.routes[route_id] = {'id:': self.id,'short_name:': self.short_name, 'long_name:': self.long_name}
-
-    def __str__(self):
-        return f"{id(self)}:{self.id}, {id(self)}:{self.short_name}, {id(self)}:{self.long_name}"
+our_data_stops={}
+our_data_stop_times=[]
+our_data_trips={}
+our_data_routes={}
 
 class Stop(object):
     def __init__ (self, stop_id, stop_name):
         self.id=stop_id
         self.name=stop_name
+
+class Route(object):
+    def __init__(self, route_id, route_short_name, route_long_name):
+        self.id = route_id
+        self.short_name = route_short_name
+        self.long_name = route_long_name
+        #self.routes[route_id] = {'id:': self.id,'short_name:': self.short_name, 'long_name:': self.long_name}
+
+    def __str__(self):
+        return f"{id(self)}:{self.id}, {id(self)}:{self.short_name}, {id(self)}:{self.long_name}"
+
 
 class Trip(object):
     def __init__ (self, trip_id, route):
@@ -102,6 +72,37 @@ class StopSegment(object):
 
     def __str__(self):
         return f"z: {self.from_stop.name} do: {self.to_stop.name}"
+
+with open('PID_GTFS/stops.txt',encoding="utf-8", newline='') as raw_stops:
+    stops_reader = csv.DictReader(raw_stops)
+    for row in stops_reader:
+        stop = Stop(row['stop_id'], row['stop_name'])
+        our_data_stops[stop.id]= stop # !! Niektore stops nemaju jmeno, ale to asi neva
+                                                    # Sample: 'U1072S1E1081': {'jmeno': 'E8'}
+with open('PID_GTFS/routes.txt',encoding="utf-8", newline='') as raw_routes:
+    routes_reader = csv.DictReader(raw_routes)
+    for row in routes_reader:
+        route = Route(row['route_id'], row['route_short_name'], row['route_long_name'])
+        our_data_routes[route.id] = route
+        
+with open('PID_GTFS/trips.txt',encoding="utf-8", newline='') as raw_trips:
+    trips_reader = csv.DictReader(raw_trips)
+    for row in trips_reader:
+        route_pk = row['route_id']
+        trip = Trip(row['trip_id'], our_data_routes.get(route_pk))
+        our_data_trips[trip.id] = trip # Sample: '1349_28156_211212': {'linka_id': 'L1349'},
+
+with open('PID_GTFS/stop_times.txt',encoding="utf-8", newline='') as raw_stop_times:
+    stop_times_reader = csv.DictReader(raw_stop_times)
+    for row in stop_times_reader:
+        cesta=row['trip_id'] #matuce
+        zastavka=row['stop_id']
+        poradi=row['stop_sequence']
+        prichod=row['arrival_time']
+        odchod=row['departure_time']
+        our_data_stop_times.append({'cesta':cesta,'zastavka':zastavka,'poradi':poradi, 'prichod': prichod, 'odchod': odchod})
+
+
 
 StopSegment().create_segment(our_data_stop_times,our_data_trips,our_data_stops,our_data_routes)
 
