f1e46a3f0b700692dbde00e82e18891a11426dd5
dancejod <dancejova.daniela@gmail.com>
2022-05-15 15:46:58 +0200

    Getter uz nie je case sensitive

--- datagetter.py
+++ datagetter.py
@@ -4,7 +4,7 @@ import sys
 
 def get_data():
     user_input = str(input("Prajete si stiahnut nove data? Y/N "))
-    if user_input == 'Y':
+    if user_input.lower() == 'y':
         req = requests.get("http://data.pid.cz/PID_GTFS.zip")
         print("Stahovanie dokoncene.")
 
@@ -12,7 +12,7 @@ def get_data():
         zip.extractall("gtfs")
         print("Data uspesne extrahovane.")
     
-    elif user_input == 'N':
+    elif user_input.lower() == 'n':
         pass
     else:
         sys.exit("wrong answer")
