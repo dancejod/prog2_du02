d5f16ed570d3faecc6ae156eaf30010a622ac118
dancejod <dancejova.daniela@gmail.com>
2022-05-16 22:12:32 +0200

    Pridane docstringy a komentare ku hlavnemu skriptu

--- ukol2_gtfs.py
+++ ukol2_gtfs.py
@@ -2,6 +2,7 @@ import csv
 import sys
 from datetime import datetime, timedelta
 
+# Priprava potrebnych slovnikov a zoznamov
 our_data_stops={}
 our_data_stop_times=[]
 our_data_trips={}
@@ -9,6 +10,12 @@ our_data_routes={}
 our_data_services={}
 
 try:
+    '''
+    Import pomocneho skriptu datagetter.py a vykonanie metody get_data().
+    
+    Funkcia sa dotazuje na stiahnutie novych dat, ak pouzivatel suhlasi, stiahne
+    najnovsie data jizdnich radu z PID.
+    '''
     import datagetter
     datagetter.get_data()
 
@@ -25,27 +32,57 @@ except IOError:
     sys.exit("Subor datagetter.py neexistuje, alebo nie je v spravnom adresari.")
 
 def convert_user_date(date_user):
-    # konvertuje datum zadaný uživatelem (string) na objekt třídy date
+    '''
+    Funkce konvertuje datum zadany uzivatelem na objekt tridy date.
+
+        Parametry:
+            date_user (str): Datum zadany uzivatelem v terminali.
+
+        Vraci:
+            user_date_obj (objekt): Objekt tridy date.
+    '''
     date_time_obj = datetime.strptime(date_user,'%d.%m.%Y')
     user_date_obj=datetime.date(date_time_obj)
     return user_date_obj
 
 def convert_int_date(date_int):
-    # konvertuje datum ze souboru calendar.txt na objekt třídy date
+    '''
+    Funkce konvertuje datum ze souboru calendar.txt na objekt tridy date.
+
+        Parametry:
+            date_int (int): Datum ze souboru calendar.txt.
+
+        Vraci:
+            data_date_obj (objekt): Objekt tridy date.
+    '''
     date_time_obj = datetime.strptime(date_int,'%Y%m%d')
     data_date_obj=datetime.date(date_time_obj)
     return data_date_obj
 
 def daterange(start, end):
-    # vezme objekty typu date a vratí set datumů v intervalu mezi start a end
+    '''
+    Funkce vezme objekty tridy date a vrati set datumu v intervalu mezi start a end.
+
+        Parametry:
+            start (objekt): Objekt tridy date.
+            end (objekt): Objekt tridy date.
+
+        Vraci:
+            dates (set): Set datumu v intervalu <start, end>.
+    '''
     dates=set()
+    # Postupne pridavani datumu v zadanem intervalu do setu
     for n in range(int ((end - start).days)+1):
         dates.add(start + timedelta(n))
     return dates
 
 class Stop(object):
     """
-    třída objektů Stop s atributy id(str) a stop_name(str)
+    Trida objektu Stop.
+
+        Atributy:
+            id (str): ID zastavky z jizdnych radu.
+            stop_name (str): Nazev zastavky z jizdnich radu.
     """
     def __init__ (self, stop_id, stop_name):
         self.id=stop_id
@@ -53,7 +90,11 @@ class Stop(object):
 
 class Route(object):
     """
-    třída objektů Route s atributy id(str) a route_name(str)
+    Trida objektu Route.
+
+        Atributy:
+            id (str): ID linky z jizdnych radu.
+            route_name (str): Nazev linky z jizdnich radu.
     """
     def __init__(self, route_id, route_name):
         self.id = route_id
@@ -61,32 +102,49 @@ class Route(object):
 
 class Service(object):
     """
-    třída objektů Service s atributy id(str) a service_days(set) s položkami třídy date, kdy service funguje
+    Trida objektu Service.
+
+        Atributy:
+            service_id (str): ID sluzby, ktera sprovoznuje spoje.
+            service_days (set): Set objektu tridy date, kdy dana sluzba jezdi.
+    
+        Metody:
+            get_service (class metoda): Vrati objekt tridy Service, kde je parametrem
+            radek z csv.DictReaderu.
     """
     def __init__(self,service_id,service_days):
         self.id=service_id
-        self.service_days=service_days # list objektu tridy date, kdy danej service jezdi
+        self.service_days=service_days
 
     @classmethod
     def get_service(cls,service_row):
-        # vrátí objekt třídy Service, parametrem je řádek z csv.DictReaderu
-        service_days = set() # sem se budou ukládat dny, kdy service funguje
-        start,end = convert_int_date(service_row['start_date']),convert_int_date(service_row['end_date']) # konverze datumů ze souboru na objekt třídy date
-        service_dates = daterange(start,end) # seznam datumů získaný z intervalu mezi start a end
+        """ Vrati objekt tridy Service, kde je parametrem radek z csv.DictReaderu."""
+        # Ukladani dnu, kdy sluzba funguje
+        service_days = set()
+        #Konverze datumu ze souboru na objekt tridy date
+        start,end = convert_int_date(service_row['start_date']),convert_int_date(service_row['end_date'])
+        # Vytvoreni setu datumu ziskany z intervalu <start; end>
+        service_dates = daterange(start,end)
         week_service_list=[service_row['monday'],service_row['tuesday'],service_row['wednesday'],service_row['thursday'],service_row['friday'],service_row['saturday'],service_row['sunday']]
-        # seznam, ve kterém je uloženo, jaké dny v týdnu funguje service, když funguje, tak položka je '1'
+        # Seznam, ve kterém je uloženo, jaké dny v týdnu funguje service, když funguje, tak položka je '1'
         for date in service_dates:
-            # iterace přes dny v odpovídajícím intervalu
-            day_in_week_int=date.weekday() # uloží se den v týdnu podle data (0-6)
+            # Iterace přes dny v odpovídajícím intervalu, uloží se den v týdnu podle data (0-6)
+            day_in_week_int=date.weekday()
+            # Když service funguje daný den v týdnu, tak ho přidáme do setu dní, kdy vybraný service funguje
             if week_service_list[day_in_week_int] == '1': 
-                # když service funguje daný den v týdnu, tak ho přidáme do setu dní, kdy vybraný service funguje
                 service_days.add(date)
-        service_complete=Service(service_row['service_id'],service_days) # sem se uloží objekt třídy Service
-        return service_complete # vrátíme objekt třídy Service
+        # Ulozeni noveho objektu tridy Service a jeho vraceni
+        service_complete=Service(service_row['service_id'],service_days) 
+        return service_complete 
 
 class Trip(object):
     """
-    objekt třídy Trip s atributy id(str), route(Route) a service(Service)
+    Trida objektu Trip.
+
+        Atributy:
+            trip_id (str): ID spoje z jizdnich radu.
+            route (objekt): Objekt tridy Route.
+            service (objekt): Objekt tridy Service.
     """
     def __init__ (self, trip_id, route, service):
         self.id=trip_id
@@ -95,7 +153,12 @@ class Trip(object):
 
 class StopTime(object):
     """
-    objekt třídy StopTime s atributy trip(Trip), stop(Stop) a stop_sequence(str)
+    Trida objektu StopTime.
+
+        Atributy:
+            trip (objekt): Objekt tridy Trip.
+            stop (objekt): Objekt tridy Stop.
+            stop_sequence (str): Poradi zastavky v spoji.
     """
     def __init__(self, trip, stop, stop_sequence):
         self.trip=trip
@@ -104,10 +167,29 @@ class StopTime(object):
 
 class StopSegment(object):
     """
-    objekt třídy StopSegment s atributy from_stop(Stop), to_stop(Stop), trips(list) a routes(set)
-    vždy je definovaný dvojicí from_stop a to_stop
+    Trida objektu StopSegment.
+
+        Atributy:
+            from_stop (objekt): Objekt tridy Stop.
+            to_stop (objekt): Objekt tridy Stop.
+            trip (list): Seznam spoju prochazejicich mezi from_stop a to_stop.
+            route (objekt): Set linek prochazejicich mezi from_stop a to_stop.
+        
+        Metody:
+            get_segment_dict(class metoda): Vrati slovnik, kde klicem je dvojice ID za sebou jdoucich zastavek
+                a hodnotou objekt tridy StopSegment.
+
+                Parametry:
+                    data_stop_times (list): Seznam objektu tridy StopTime.
+                    date_string (str): Datum ve formatu DD.MM.RRRR zadane uzivatelem.
+
+            print_trip_count_from_segments(class metoda): Seřadí slovník segmentů podle délky seznamu tripů jím projíždějících 
+                a vytiskne 5. nejfrekventovanějších.
+
+                Parametry:
+                    segment_dict (dict): Slovnik segmentu ziskanych v metode get_segmet_dict.
     """
-    def __init__(self, from_stop, to_stop, trip,route):
+    def __init__(self, from_stop, to_stop, trip, route):
         self.from_stop = from_stop
         self.to_stop = to_stop
         self.trips = [trip]
@@ -115,50 +197,61 @@ class StopSegment(object):
         
     @classmethod
     def get_segment_dict(cls, data_stop_times,date_string):
-        # vrátí slovník, kde klíčem je dvojice from_stop.id a to_stop.id a hodnotou objekt třídy StopSegment
-        # parametrem je seznam objektů třídy StopTime a datum ve formátu DD.MM.RRRR zadané uživatelem
-        user_date=convert_user_date(date_string) # konverze date_string na objekt třídy date
+        """Vrati slovnik, kde klicem je dvojice ID za sebou jdoucich zastavek
+        a hodnotou objekt tridy StopSegment."""
+        # Konverze date_string na objekt tridy date
+        user_date=convert_user_date(date_string)
         segment_dict={}
         for current_stop_time in data_stop_times:
+            # Kdyz datum zadane uzivatelem neni v seznamu datumu kdy funguje sluzba,
+            # ktera odpovida spoji, cyklus pokracuje s dalsi iteraci
             if user_date not in current_stop_time.trip.service.service_days:
-                # když je datum zadané uživatelem není seznamu datumů, kdy funguje service odpovídající tripu odpovídající current_stop_time
-                continue # pokračujeme s další iterací
-            # jinak se cyklus proběhne
+                continue
+            # Jinak cyklus probehne a bude se pokouset vytvaret objekt typu StopSegment
+            # Kdyz je zastavka prvni na spoji, priradime prave iterovanou zastavku vychozi zastavce
+            # a cyklus pokracuje s dalsi iteraci
             if current_stop_time.stop_sequence == '1':
-                # když je zastávka první na tripu, přiřadíme zastávku z current_stop_time výchozí zastávce
                 from_stop = current_stop_time.stop
                 continue
+            # Kdyz je zastavka druha na spoji, priradime prave iterovanou zastavku cilove zastavce
             elif current_stop_time.stop_sequence == '2':
-                # když zastávka 2. na tripu, přiřadíme zastávku z current_stop_time cílové zastávce
                 to_stop = current_stop_time.stop
+            # Jinak se z cílové stane výchozí a cílové přiřadíme aktualni zastavku
             else:
-                # jinak se z cílové stane výchozí a cílové přiřadíme zastávku z current_stop_time
                 from_stop = to_stop
                 to_stop = current_stop_time.stop
-            segment_key=(from_stop.id,to_stop.id) # vytvoříme segment_key z id obou zastávek, což je jednoznačný identifikátor segmentů
-            trip=current_stop_time.trip # objekt třídy Trip uložíme do proměnné
-            route=trip.route.name # objekt třídy Route uložíme do proměnné
+            # Vytvorime segment_key z ID obou zastavek, ktery jednoznacne identifikuje segment
+            segment_key=(from_stop.id,to_stop.id)
+            # Objekt tridy Trip pro aktualni zastavku ulozime do promenne
+            trip=current_stop_time.trip
+            # Objekt třídy Route pro aktualni spoj uložíme do proměnné
+            route=trip.route.name 
+            # Kdyz segment_key jeste neni klicem ve slovniku segmentu,
+            # vytvorime objekt tridy StopSegment, ktery vezme jako parametry
+            # promenne vytvorene behem aktualni iterace
             if segment_key not in segment_dict:
-            # když segment_key jestě není jako klíč ve slovníku segmentů:
-                segment=StopSegment(from_stop,to_stop,trip,route) # vytvoříme objekt třídy StopSegment, který vezme jako parametry proměnné vytvořené během této iterace
-                segment_dict[(segment_key)]=segment # objekt třídy StopSegment uložíme jako hodnotu do slovníku
+                segment=StopSegment(from_stop,to_stop,trip,route)
+                # Objekt třídy StopSegment uložíme jako hodnotu do slovníku
+                segment_dict[(segment_key)]=segment 
             else:
-            # když ve slovníku už segment_key je:
-                segment_dict[segment_key].trips.append(trip) # přidáme odpovídající trip do seznamů tripů odpovídajícího segmentu
-                # pokud v slovníku linek odpovídajího segmentu není aktuální linka uložena, tak jí tam přidáme
+            # Když ve slovníku už segment_key je,
+            # přidáme odpovídající trip do seznamů tripů odpovídajícího segmentu,
+            # pokud v slovníku linek odpovídajího segmentu není aktuální linka uložena, tak jí tam přidáme
+                segment_dict[segment_key].trips.append(trip) 
                 segment_dict[segment_key].routes.add(route)
-        return segment_dict # vratíme cely slovník
+        # Na závěr vrátime cely slovník
+        return segment_dict 
     
-
     @classmethod
     def print_trip_count_from_segments(cls, segment_dict):
-        # seřadí slovník segmentů podle délky seznamu tripů jím projíždějících a vytiskne 5. nejfrekventovanějších
+        # Seřadení slovník segmentů podle délky seznamu tripů jím projíždějících a vytiskne 5. nejfrekventovanějších
         sorted_segment_dict = sorted(segment_dict.values(), key = lambda x: len(x.trips), reverse=True)
         i = 1
         for item in sorted_segment_dict[:5]:
             print(f"{i}. nejfrekventovanější úsek je mezi zastávkami {item.from_stop.name} a {item.to_stop.name}. Projede jím {len(item.trips)} spojů linek {', '.join(sorted(item.routes))}.")
             i+=1
-        
+
+# Otevreni potrebnych souboru pro chod programu
 try:
     stops = "stops.txt"
     with open("gtfs/"+stops, encoding="utf-8", newline='') as raw_stops:
@@ -192,7 +285,7 @@ except PermissionError:
 try:
     calendar = 'calendar.txt'        
     with open("gtfs/"+calendar, encoding="utf-8", newline='') as raw_calendar:
-        # otevření souboru calendar.txt a uložení potřebných dat do slovníku za použití námi implementované metody
+        # otevření souboru calendar.txt a uložení potřebných dat do slovníku za použití implementované metody v tride Service
         calendar_reader = csv.DictReader(raw_calendar)
         for row in calendar_reader:
             service=Service.get_service(row)
@@ -238,7 +331,9 @@ except FileNotFoundError:
 except PermissionError:
     sys.exit(f"K suboru {stop_times} nemate pozadovane prava.")
 
-#datum=str(input("zadejte datum ve formátu dd.mm.rrrr "))
+# Sprovozneni programu, uzivatel musi otevrit skript v terminalu
+# zadanim python ./ukol2_gtfs.py DD.MM.RRRR
+# Jinak se program ukonci
 try:    
     user_date = StopSegment.get_segment_dict(our_data_stop_times, sys.argv[1])
 
@@ -248,4 +343,5 @@ except ValueError:
 except IndexError:
     sys.exit("Nezadali ste datum, pre ktory chcete vygenerovat najfrekventovanejsie useky.")
 
+# Vytisknuti nejfrekventovanejsich linek v zadany den
 StopSegment.print_trip_count_from_segments(user_date)
